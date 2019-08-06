89b43a320457ebc090f42efd0dce138188c74e73
TscholponChicago <TscholponChicago>
2019-08-06 18:45:33 -0500

    some practice

--- src/JavaClass1.java
+++ src/JavaClass1.java
@@ -1,5 +1,8 @@
 public class JavaClass1 {
     public static void main(String[] args) {
         System.out.println("I have kids");
+        System.out.println("What are you doing?");
+        String str1 = "International";
+        System.out.println(str1.substring(2,5));
     }
 }
